e0921fb368fd70156f545bb20a1e57717a5ac885
devlinj678 <linj67845@gmail.com>
2025-05-21 11:48:14 -0700

    Refactor project initialization: rename image processor project and adjust references for clarity

--- ImageFun.AppHost/AppHost.cs
+++ ImageFun.AppHost/AppHost.cs
@@ -11,15 +11,17 @@ var storage = builder.AddAzureStorage("storage").RunAsEmulator();
 var blobs = storage.AddBlobs("blobs");
 var container = blobs.AddBlobContainer("images", blobContainerName: "image-uploads");
 
-builder.AddProject<Projects.ImageUpload>("web")
-    .WithExternalHttpEndpoints()
-    .WithReference(container)
-    .WaitFor(container);
-
-builder.AddProject<Projects.ImageProcessor>("imageprocessor")
+var imageProcessor = builder.AddProject<Projects.ImageProcessor>("image-processor")
        .WithExternalHttpEndpoints()
        .WithReference(container)
        .WithReference(oaics)
        .WaitFor(container);
+
+builder.AddProject<Projects.ImageUpload>("web")
+    .WithExternalHttpEndpoints()
+    .WithReference(container)
+    .WaitFor(container)
+    .WithReference(imageProcessor)
+    .WaitFor(imageProcessor);
 
 builder.Build().Run();
